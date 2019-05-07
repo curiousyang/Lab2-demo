d008d59e4d6480fb95ca00bef5d570e11ebc0ea0
Yang Song <yangsong531@gmail.com>
2019-05-06 20:37:22 -0700

    initial file setup

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var likesGamesOfThrones = prompt('Do you like Game of Thrones?');
+
+likesGamesOfThrones = likesGamesOfThrones.toLowerCase();
+
+if (likesGamesOfThrones ==='yes' ){
+    alert('me too!');
+} else {
+    alert('how can you not like Game of Thrones?');
+}
+
+
+var coding = prompt('Do you like coding');
+coding = coding.toLowerCase();
+if (coding ==='yes' ){
+    alert('You are smart');
+} else {
+    alert('Ops.. that sucks');
+}
